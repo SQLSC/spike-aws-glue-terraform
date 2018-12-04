dd4eb0539e57bd07af7fbdf0f02a2fd6b2ab7b75
Dariusz Bielak <dariusz.bielak@sqlsc.com>
2018-12-04 20:36:46 +0000

    Added the missing name to the kms alias

--- terraform/key-management-service/kms.tf
+++ terraform/key-management-service/kms.tf
@@ -3,7 +3,7 @@ resource "aws_kms_key" "zone_key" {
   deletion_window_in_days = 7
 }
 
-resource "aws_kms_alias" "" {
+resource "aws_kms_alias" "zone_key_alias" {
   provider = "${var.provider}"
   target_key_id = "${aws_kms_key.zone_key.id}"
   name = "${var.zone_key_name}"
