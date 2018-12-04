412ff5dadb3d7010dfe0a6bc8af8e1a0bfffde54
Dariusz Bielak <dariusz.bielak@sqlsc.com>
2018-12-04 20:35:18 +0000

    Added provider var to module

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,6 +1,6 @@
 module "kms" {
   source = "key-management-service"
-
+  provider = "${var.provider}"
   region = "${var.region}"
   zone_key_name = "${var.key_zone}"
 }
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,2 +1,3 @@
 variable "region" {}
 variable "key_zone" {}
+variable "provider" {}
