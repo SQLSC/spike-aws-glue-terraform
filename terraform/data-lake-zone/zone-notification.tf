b8625d164ecb2282c07fbdd21bd570fc29174421
Dariusz Bielak <dariusz.bielak@sqlsc.com>
2018-12-05 05:53:24 +0000

    Fix bucket name

--- terraform/data-lake-zone/zone-notification.tf
+++ terraform/data-lake-zone/zone-notification.tf
@@ -1,5 +1,5 @@
 resource "aws_s3_bucket_notification" "default_notification" {
-  bucket = "${aws_s3_bucket.dl-zone.arn}"
+  bucket = "${aws_s3_bucket.dl-zone.id}"
 
   lambda_function {
     lambda_function_arn = "${var.lambda_function_arn}"
